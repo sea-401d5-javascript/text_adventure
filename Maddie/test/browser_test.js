f5b7adf56a1e0a9635c84c3dd219ca4bf9f9cd8b
jdar22169 <mmow313@gmail.com>
2016-06-16 12:06:52 -0700

    fixed gulpfile.js

--- Maddie/test/browser_test.js
+++ Maddie/test/browser_test.js
@@ -2,7 +2,6 @@
 const angular = require('angular');
 require('angular-mocks');
 require('../app/js/client.js');
-require('eslint-plugin-jasmine');
 
 describe('Controller Tests', () => {
   let storyCtrl;
