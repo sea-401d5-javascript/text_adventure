479a43b86658247717b04a16a57d2b5d1881b6db
jdar22169 <mmow313@gmail.com>
2016-06-16 10:51:55 -0700

    testing partly done

--- Maddie/gulpfile.js
+++ Maddie/gulpfile.js
@@ -2,13 +2,20 @@
 
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const del = require('del');
+const Server = require('karma').Server;
 
 const paths = {
   html: __dirname + '/app/index.html',
   js: __dirname + '/app/js/client.js',
-  css:__dirname + '/app/css/style.css'
+  css:__dirname + '/app/css/style.css',
+  test:__dirname + '/test/*_test.js'
 
 };
+
+gulp.task('clean', () => {
+  return del('./build/**/*');
+});
 
 gulp.task('copy', () => {
   gulp.src(paths.html)
@@ -30,4 +37,21 @@ gulp.task('bundle', () => {
   .pipe(gulp.dest('./build'));
 });
 
-gulp.task('build', ['copy', 'copyCSS', 'bundle']);
+gulp.task('bundle:test', () => {
+  gulp.src(paths.test)
+  .pipe(webpack({
+    output: {
+      filename: 'test_bundle.js'
+    }
+  }))
+  .pipe(gulp.dest(__dirname + '/test'));
+});
+
+gulp.task('karma', ['bundle:test'], (done) => {
+  new Server({
+    configFile:__dirname + '/karma.conf.js'
+  }, done).start();
+});
+
+gulp.task('build', [ 'bundle','clean','copy', 'copyCSS']);
+gulp.task('test:client', ['bundle:test', 'karma']);
