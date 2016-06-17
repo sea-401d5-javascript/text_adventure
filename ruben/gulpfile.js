2b5c37edc191589d65c5f245851e3527eb2342ab
Ruben Sarkisyan <rubens24@uw.edu>
2016-06-16 21:56:43 -0700

    added testing for game app

--- ruben/gulpfile.js
+++ ruben/gulpfile.js
@@ -1,25 +1,52 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
 
-gulp.task('webpack:dev', function() {
-  return gulp.src('./app/js/client.js')
+gulp.task('copy', () => {
+  return gulp.src(__dirname + '/app/index.html')
+    .pipe(gulp.dest(__dirname + '/build'));
+});
+
+gulp.task('bundle', () => {
+  return gulp.src(__dirname + '/app/js/client.js')
     .pipe(webpack({
       output: {
         filename: 'bundle.js'
       }
     }))
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest(__dirname + '/build'));
 });
 
-gulp.task('staticfiles:dev', function() {
-  return gulp.src('./app/**/*.html')
-  .pipe(gulp.dest('build/'));
+gulp.task('bundle:test', () => {
+  return gulp.src(__dirname + '/test/*test.js')
+    .pipe(webpack({
+      output: {
+        filename: 'test_bundle.js'
+      }
+    }))
+    .pipe(gulp.dest(__dirname + '/test'))
 });
 
-gulp.task('staticcssfiles:dev', function() {
-  return gulp.src('./app/css/*.css')
-  .pipe(gulp.dest('build/'));
-});
-
-gulp.task('build:dev', ['staticfiles:dev','staticcssfiles:dev', 'webpack:dev']);
-gulp.task('default', ['build:dev']);
+gulp.task('default', ['bundle:test', 'bundle', 'copy']);
+//
+// gulp.task('webpack:dev', function() {
+//   return gulp.src('./app/js/client.js')
+//     .pipe(webpack({
+//       output: {
+//         filename: 'bundle.js'
+//       }
+//     }))
+//     .pipe(gulp.dest('build/'));
+// });
+//
+// gulp.task('staticfiles:dev', function() {
+//   return gulp.src('./app/**/*.html')
+//   .pipe(gulp.dest('build/'));
+// });
+//
+// gulp.task('staticcssfiles:dev', function() {
+//   return gulp.src('./app/css/*.css')
+//   .pipe(gulp.dest('build/'));
+// });
+//
+// gulp.task('build:dev', ['staticfiles:dev','staticcssfiles:dev', 'webpack:dev']);
+// gulp.task('default', ['build:dev']);
